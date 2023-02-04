96e019c9a1fa9c84fb05c076606d0cd557ec55ed
termuxparadevs <termuxparadevs@outlook.com>
2023-02-04 07:09:32 -0300

    adiciona menu de instalacao e desinstalacao

--- install.sh
+++ install.sh
@@ -9,31 +9,56 @@
 # Em seguida, ele baixa o script speedtest-cli, torna-o executável e cria um link simbólico para tmdspeed.
 # O objetivo deste script é facilitar a instalação da biblioteca speedtest-cli no Termux, garantindo que todas as dependências estejam instaladas antes de baixar o script.
 
+#!/bin/bash
 
-# Verifica se o curl está instalado
-if ! command -v curl &> /dev/null
-then
-    # Instala o curl
-    pkg in curl
-fi
-
-# Verifica se o python estpa instalado
-if ! command -v python3 $> /dev/null
-then
-    # Instala o Python3
-    pkg in python3
-fi
-
-curl -Lo $PREFIX/bin/speedtest-cli https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py
-chmod +x $PREFIX/bin/speedtest-cli
-ln -s $PREFIX/bin/speedtest-cli $PREFIX/bin/tmdspeed
-
+clear
+echo "###################################################"
+echo "#                                                 #"
+echo "#       Bem vindo ao Instalador/Desinstalador     #"
+echo "#                  do tmdspeed                    #"
+echo "#                                                 #"
+echo "###################################################"
 echo ""
-echo -e "\033[0;32mTMD - Arquivo tmdspeed baixado com sucesso!\033[0m\n"
+echo "Escolha uma opção:"
+echo "1) Instalar"
+echo "2) Desinstalar"
+echo ""
+read -p "Opção: " opcao
 
-SIZE=$(du -h $PREFIX/bin/speedtest-cli | awk '{print $1}')
-echo "O tamanho do arquivo baixado foi de $SIZE"
+if [ $opcao == 1 ]; then
+  # Código para instalação
+  if command -v python3 > /dev/null 2>&1; then
+    echo "Python3 já está instalado."
+  else
+    echo "Instalando python3..."
+    pkg install python3 -y
+  fi
 
-# Utilização:
-# Depois de rodar o script install.sh, você pode executar o comando "tmdspeed" a partir de qualquer diretório no seu terminal Termux. 
-# Isso irá rodar a biblioteca speedtest-cli e fornecer as informações sobre a velocidade da sua conexão com a internet.
+  if command -v curl > /dev/null 2>&1; then
+    echo "Curl já está instalado."
+  else
+    echo "Instalando curl..."
+    pkg install curl -y
+  fi
+
+  echo "Baixando arquivo..."
+  curl -Lo speedtest-cli https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py
+  chmod +x speedtest-cli
+  mv speedtest-cli $PREFIX/bin/
+  ln -s $PREFIX/bin/speedtest-cli $PREFIX/bin/tmdspeed
+  echo ""
+  echo -e "\033[32mInstalação concluída com sucesso!\033[0m"
+  echo "O tamanho do arquivo baixado é $(du -sh $PREFIX/bin/speedtest-cli | awk '{print $1}')"
+
+elif [ $opcao == 2 ]; then
+  # Código para desinstalação
+  echo "Removendo arquivo..."
+  rm $PREFIX/bin/speedtest-cli
+  rm $PREFIX/bin/tmdspeed
+  echo ""
+  echo -e "\033[32mDesinstalação concluída com sucesso!\033[0m"
+
+else
+  echo "Opção inválida."
+  exit 1
+fi
